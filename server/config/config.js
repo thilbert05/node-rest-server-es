d8d8450eb7635a347d5a7ec16a12d7ecf09f9ba5
Tomas <tomashilbert@hotmail.com>
2021-01-24 17:33:43 -0500

    Variable de Entorno MONGO PROD

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ENTORNO
 //=============
 // process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 
-let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://thilbert:JDg6Kn3yOsObkUWV@nodecluster.qo9m6.mongodb.net/cafe';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 process.env.URLDB = urlDB;
 
