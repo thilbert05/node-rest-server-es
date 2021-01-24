af2d799839da7d58474b4a02622841f3acdde411
Tomas <tomashilbert@hotmail.com>
2021-01-24 17:15:55 -0500

    REST SERVER Working

---
+++ server/models/usuario.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const rolesValidos = {
+  values: ['ADMIN_ROLE', 'USER_ROLE'],
+  message: '{VALUE} no es un role válido'
+}
+
+const usuarioSchema = new Schema({
+  nombre: {
+    type: String,
+    required: [true, 'El nombre es un campo obligatorio']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'El correo es un campo obligatorio']
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria']
+  },
+  img: {
+    type: String,
+    
+  },
+  role: {
+    type: String,
+    default: 'USER_ROLE',
+    enum: rolesValidos
+  },
+  estado: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  },
+});
+
+usuarioSchema.methods.toJSON = function() {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
+  return userObject;
+}
+
+usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser único'})
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
